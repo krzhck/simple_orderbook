0a5bd8974d25a2d9a8563ebbb52ec2067c34c910
krzhck <isaaxou@icloud.com>
2023-12-21 13:31:09 +0800

    main cleanup

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 int main()
 {
+    // test   
     Orderbook* book = new Orderbook();
-
-
 
     book->AddOrder(1, 100, 10, OrderType::BUY);
     book->AddOrder(2, 200, 10, OrderType::BUY);
@@ -15,20 +14,16 @@ int main()
     book->AddOrder(6, 200, 10, OrderType::SELL);
     book->AddOrder(7, 300, 10, OrderType::SELL);
     book->AddOrder(8, 400, 10, OrderType::SELL);
-
+    book->AddOrder(9, 400, 100, OrderType::BUY);
     book->AddOrder(10, 1000, 250, OrderType::BUY);
-
     book->AddOrder(11, 10, 10, OrderType::SELL);
 
+    book->PrintSingleOrder(5);
     book->PrintAllOrders();
     book->PrintAllPriceLevels();
-    
-    //book->PrintSingleOrder(5);
 
     book->WithdrawOrder(1);
-    //book->WithdrawOrder(3);
-
-    //book->PrintAllOrders();
+    book->WithdrawOrder(3);
 
     book->Match(10);
     book->Match(11);
